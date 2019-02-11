965082dc687037f2ba4f42ef0172c88fcba969b3
守 小室 <hipukon2@gmail.com>
2019-02-11 21:57:46 +0900

    full coding

--- ConsoleTry/Program.cs
+++ ConsoleTry/Program.cs
@@ -5,6 +5,17 @@ namespace ConsoleTry
 {
     public class Janken
     {
+        public string poi(string p1, string p2)
+        {
+            if(p1=="goo"&&p2=="paa" ||
+               p1=="thoki"&&p2=="paa")
+            return "p1";
+            else
+            {
+                return "p2";
+            }
+        }
+
         public static void Main(string[] args)
         {
 
--- XUnitTestProject1/UnitTest1.cs
+++ XUnitTestProject1/UnitTest1.cs
@@ -9,14 +9,20 @@ namespace XUnitTestProject1
         [Fact]
         public void goo_tai_paa_p1_win()
         {
+            var j = new Janken();
+            Assert.True(j.poi("goo", "paa")== "p1");
         }
-        //[Fact]
-        //public void thoki_tai_paa_p1_win()
-        //{
-        //}
-        //[Fact]
-        //public void paa_tai_goo_p2_win()
-        //{
-        //}
+        [Fact]
+        public void thoki_tai_paa_p1_win()
+        {
+            var j = new Janken();
+            Assert.True(j.poi("thoki","paa")=="p1");
+        }
+        [Fact]
+        public void paa_tai_goo_p2_win()
+        {
+            var j =new Janken();
+            Assert.True(j.poi("paa","goo")=="p2");
+        }
     }
 }
